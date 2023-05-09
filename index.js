c11df9136507b735abbb9ce700e2c1348e4f9527
yousef7722 <jonabilmc@gmail.com>
2023-05-10 01:25:46 +0300

    contact

--- index.js
+++ index.js
@@ -29,3 +29,45 @@ aboutBtn.addEventListener("click", () => {
 contactBtn.addEventListener("click", () => {
   swiper1.slideTo(2);
 });
+
+const inName = document.getElementById("name");
+const inEmail = document.getElementById("email");
+const inMessage = document.getElementById("message");
+function sendMail() {
+  var params = {
+    name: inName.value,
+    email: inEmail.value,
+    message: inMessage.value,
+  };
+  emailjs
+    .send("service_olca98c", "template_33wsost", params)
+    .then((res) => {
+      inName.value = "";
+      inEmail.value = "";
+      inMessage.value = "";
+    })
+    .catch((err) => console.log(err));
+}
+
+document.querySelector("#submit").addEventListener("click", (e) => {
+  if (
+    inName.checkValidity() &&
+    inEmail.checkValidity() &&
+    inMessage.checkValidity()
+  ) {
+    e.preventDefault();
+    sendMail();
+  }
+});
+function suc(e) {
+  console.log(e);
+}
+console.log(navigator.geolocation.getCurrentPosition(suc, suc));
+//////////////////
+var map = L.map("map").setView([31.03, 31.389], 14);
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+var marker = L.marker([31.015, 31.389]).addTo(map);
